bfb869c68a3c42ec17c1835138c80207535eda50
Nathan Lawrence <lawrencen@missouri.edu>
2016-02-15 15:05:20 -0600

    Data!

--- 2016/feb/mizzou-state-funding/gulpfile.js
+++ 2016/feb/mizzou-state-funding/gulpfile.js
@@ -4,12 +4,12 @@ var gulp = require('gulp'),
 
 
 
-gulp.task('upload', shell.task([
-	'aws s3 cp * s3://media.kbia.org/2016/feb/mizzou-state-fund/ --recursive'
-]));
+gulp.task('upload', function(){
+	shell.task(['aws s3 cp index.html s3://media.kbia.org/2016/feb/mizzou-state-fund/']);
+});
 
 
 gulp.task('watch',function(){
 	gutil.log('Gulp will say that this task has finished, but don\'t believe its dirty lies.');
 	gutil.log('Hit \^c to actually exit watch mode.');
-});
+})
